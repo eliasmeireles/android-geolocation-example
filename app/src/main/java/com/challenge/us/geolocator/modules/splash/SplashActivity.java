f85334f0b0fd990a4dbf6060c6bfd17582d968a7
Elias Meireles <elias.ferreira@groupsoftware.com.br>
2021-04-15 23:09:28 -0300

    Adicionando animação no texto da Splash Screen

--- app/src/main/java/com/challenge/us/geolocator/modules/splash/SplashActivity.java
+++ app/src/main/java/com/challenge/us/geolocator/modules/splash/SplashActivity.java
@@ -3,6 +3,10 @@ package com.challenge.us.geolocator.modules.splash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.challenge.us.geolocator.R;
 
@@ -12,5 +16,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TextView textView = findViewById(R.id.activity_splash_text_view);
+        Animation animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.splash_text_animation_in);
+        textView.setAnimation(animationFadeIn);
+        textView.startAnimation(animationFadeIn);
+        textView.setVisibility(View.VISIBLE);
     }
 }
