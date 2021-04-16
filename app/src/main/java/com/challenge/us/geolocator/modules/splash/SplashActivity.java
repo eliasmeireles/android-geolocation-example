8ad0ab3ddb9028f9d4239744ac2fa4f9bff337f7
Elias Meireles <elias.ferreira@groupsoftware.com.br>
2021-04-15 22:31:38 -0300

    Layout da Splash Screen

--- app/src/main/java/com/challenge/us/geolocator/modules/splash/SplashActivity.java
+++ app/src/main/java/com/challenge/us/geolocator/modules/splash/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.challenge.us.geolocator;
+package com.challenge.us.geolocator.modules.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.challenge.us.geolocator.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
