9ce63c008a732b014b8ca81ecb494b4f499f7c33
Elias Meireles <eliasmflilico@gmail.com>
2021-04-18 11:28:47 -0300

    Refatoração splash  screen - Implementação do BindingView

--- app/src/main/java/com/challenge/us/geolocation/app/modules/splash/SplashActivity.java
+++ app/src/main/java/com/challenge/us/geolocation/app/modules/splash/SplashActivity.java
@@ -1,14 +1,15 @@
 package com.challenge.us.geolocation.app.modules.splash;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.challenge.us.geolocation.R;
+import com.challenge.us.geolocation.databinding.ActivitySplashBinding;
 
 import javax.inject.Inject;
 
@@ -19,21 +20,22 @@ public class SplashActivity extends AppCompatActivity implements SplashView {
 
     @Inject
     SplashPresenter splashPresenter;
+    private ActivitySplashBinding splashBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        splashBinding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(splashBinding.getRoot());
         splashPresenter.bindWith(this);
         setupSplashAnimation();
     }
 
     private void setupSplashAnimation() {
-        TextView animatedSplashTextView = findViewById(R.id.activity_splash_text_view);
         Animation animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.splash_text_animation_in);
-        animatedSplashTextView.setAnimation(animationFadeIn);
-        animatedSplashTextView.startAnimation(animationFadeIn);
-        animatedSplashTextView.setVisibility(View.VISIBLE);
+        splashBinding.activitySplashTextView.setAnimation(animationFadeIn);
+        splashBinding.activitySplashTextView.startAnimation(animationFadeIn);
+        splashBinding.activitySplashTextView.setVisibility(View.VISIBLE);
     }
 
     @Override
